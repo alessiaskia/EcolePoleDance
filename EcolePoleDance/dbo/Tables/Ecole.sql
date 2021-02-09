d10ee40563e15b1a1c62b9772f2fe8666c79927b
alessiaskia <a.scaccia@interface3.be>
2021-02-09 11:02:51 +0100

    added address to table Ecole

--- EcolePoleDance/dbo/Tables/Ecole.sql
+++ EcolePoleDance/dbo/Tables/Ecole.sql
@@ -4,6 +4,9 @@
 	[Email] VARCHAR(256) NOT NULL,
 	[Tel] VARCHAR(16) NOT NULL,
 	[CompteBancaire] VARCHAR(32) NOT NULL,
+	[Rue] VARCHAR(128) NOT NULL,
+	[Numero] VARCHAR(8) NOT NULL,
+	[Ville] VARCHAR(32) NOT NULL,
 
 	CONSTRAINT PK_Ecole PRIMARY KEY ([IdEcole]),
 	CONSTRAINT CK_Ecole_Email CHECK ([Email] LIKE '___%@___%.__%'),
